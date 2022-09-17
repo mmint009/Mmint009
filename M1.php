1126b5cc0543c330f09d26ef3b44a3c98d34276a
mmint009 <mintjivaluck@gmail.com>
2022-09-17 03:21:47 -0700

    first commit

---
+++ M1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P>M1.1</P>
+    <P>M1.2</P>
+    <P>M1.3</P>
+</body>
+</html>
